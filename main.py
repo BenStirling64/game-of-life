d2066f2cf2c6468baa11682df007b491b9e2ff3e
BenStirling64 <bsti0666@uni.sydney.edu.au>
2024-06-08 10:54:05 +1000

    Added the cell size as a command line argument

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def main():
         except ValueError:
             print("The width and height must be integer values.")
             return
+    elif len(sys.argv) == 4:
+        try:
+            board = Board(width=int(sys.argv[1]), height=int(sys.argv[2]), zoom=int(sys.argv[3]))
+        except ValueError:
+            print("The width, height and cell size must be integer values.")
+            return
     else:
         print("Usage: python3 main.py <width> <height>")
         return
